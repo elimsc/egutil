b6dcbb231bb1aefb73720773c6273fbdb951e266
elimsc <zhjiang.rtc@gmail.com>
2020-03-05 23:47:35 +0800

    password hash

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/elimsc/egu
+
+go 1.13
+
+require (
+	github.com/go-chi/chi v4.0.3+incompatible
+	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
+	upper.io/db.v3 v3.6.4+incompatible
+)
---
+++ password_util.go
@@ -0,0 +1,17 @@
+package egu
+
+import "golang.org/x/crypto/bcrypt"
+
+// https://gowebexamples.com/password-hashing/
+
+// HashPassword hash密码
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// CheckPasswordHash 验证密码
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
