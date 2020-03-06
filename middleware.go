e081ffe1cba6065fdf767a2f72b6a507c0d389ff
elimsc <zhjiang.rtc@gmail.com>
2020-03-06 09:24:04 +0800

    middleware chain

---
+++ middleware.go
@@ -0,0 +1,28 @@
+package egu
+
+import "net/http"
+
+// Middleware type
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+// NewMiddleware create middleware
+func NewMiddleware(f http.HandlerFunc) Middleware {
+	middleware := func(next http.HandlerFunc) http.HandlerFunc {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			f(w, r)
+			next(w, r)
+		}
+
+		return handler
+	}
+
+	return middleware
+}
+
+// Chain applies middlewares to a http.HandlerFunc
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
